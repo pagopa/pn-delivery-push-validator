b816af4e51e320db734772110ca7f5158c98031c
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-07-23 13:05:07 +0200

    [PN-15774] feat: fix

---
+++ src/test/java/it/pagopa/pn/deliverypushvalidator/middleware/dao/dynamo/mapper/DtoToEntityDocumentCreationRequestMapperTest.java
@@ -0,0 +1,39 @@
+package it.pagopa.pn.deliverypushvalidator.middleware.dao.dynamo.mapper;
+
+
+import it.pagopa.pn.deliverypushvalidator.dto.documentcreation.DocumentCreationRequest;
+import it.pagopa.pn.deliverypushvalidator.dto.documentcreation.DocumentCreationTypeInt;
+import it.pagopa.pn.deliverypushvalidator.middleware.dao.documentcreationdao.dynamo.entity.DocumentCreationRequestEntity;
+import it.pagopa.pn.deliverypushvalidator.middleware.dao.documentcreationdao.dynamo.mapper.DtoToEntityDocumentCreationRequestMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DtoToEntityDocumentCreationRequestMapperTest {
+    private DtoToEntityDocumentCreationRequestMapper mapper;
+
+    @BeforeEach
+    void before() {
+        mapper = new DtoToEntityDocumentCreationRequestMapper();
+    }
+    
+    @Test
+    void dto2Entity() {
+
+        DocumentCreationRequest request = DocumentCreationRequest.builder()
+                .iun("iun")
+                .timelineId("timelineId")
+                .recIndex(1)
+                .key("keyY")
+                .documentCreationType(DocumentCreationTypeInt.SENDER_ACK)
+                .build();
+
+        DocumentCreationRequestEntity entity = mapper.dto2Entity(request);
+
+        Assertions.assertEquals(request.getIun(), entity.getIun());
+        Assertions.assertEquals(request.getKey(), entity.getKey());
+        Assertions.assertEquals(request.getTimelineId(), entity.getTimelineId());
+        Assertions.assertEquals(request.getDocumentCreationType(), DocumentCreationTypeInt.valueOf(entity.getDocumentType()) );
+        Assertions.assertEquals(request.getRecIndex(), entity.getRecIndex());
+    }
+}
