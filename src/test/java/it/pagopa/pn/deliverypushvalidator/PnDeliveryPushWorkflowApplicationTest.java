77b64ce83bf81d903b38a19c79a3b6bf933b7b3c
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-07-24 14:55:53 +0200

    [PN-15776] feat: [WI-DP-F4-1.8] - Introduzione Spring Analyzer

---
+++ src/test/java/it/pagopa/pn/deliverypushvalidator/PnDeliveryPushWorkflowApplicationTest.java
@@ -0,0 +1,23 @@
+//package it.pagopa.pn.deliverypushvalidator;
+
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringApplication;
+
+
+
+
+//@ExtendWith(MockitoExtension.class)
+//class PnDeliveryPushWorkflowApplicationTest {
+//
+//    @Test
+//    void testBuildSpringApplicationWithListener() {
+//        SpringApplication app = PnDeliveryPushValidatorApplication.buildSpringApplicationWithListener();
+//        assertNotNull(app, "SpringApplication non deve essere null");
+//
+//        boolean listenerFound = app.getListeners().stream()
+//                .anyMatch(TaskIdApplicationListener.class::isInstance);
+//        assertTrue(listenerFound, "TaskIdApplicationListener deve essere aggiunto alla SpringApplication");
+    }
+}
