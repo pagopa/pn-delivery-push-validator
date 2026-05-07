9bda6a2fe12b7d0a4db9ffcb42a847c4fa0c4bea
DacunzoMa <matteo.dacunzo@emeal.nttdata.com>
2026-05-07 17:34:40 +0200

    fix: update import for CommunicationType

--- src/main/java/it/pagopa/pn/deliverypushvalidator/dto/ext/delivery/notification/NotificationInt.java
+++ src/main/java/it/pagopa/pn/deliverypushvalidator/dto/ext/delivery/notification/NotificationInt.java
@@ -1,6 +1,6 @@
 package it.pagopa.pn.deliverypushvalidator.dto.ext.delivery.notification;
 
-import it.pagopa.pn.api.dto.events.CommunicationType;
+import it.pagopa.pn.deliverypushvalidator.dto.timeline.CommunicationType;
 import it.pagopa.pn.deliverypushvalidator.generated.openapi.msclient.delivery.model.NotificationFeePolicy;
 import lombok.*;
 import java.time.Instant;
