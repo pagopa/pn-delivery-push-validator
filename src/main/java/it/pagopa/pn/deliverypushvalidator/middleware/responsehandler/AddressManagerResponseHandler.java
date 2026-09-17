6dc9d920563e9799e3d5bff572f9fd7ef8e8c0d4
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2026-09-17 11:25:38 +0200

    [PN-21447] - restored AddressManagerResponseHandler log message

--- src/main/java/it/pagopa/pn/deliverypushvalidator/middleware/responsehandler/AddressManagerResponseHandler.java
+++ src/main/java/it/pagopa/pn/deliverypushvalidator/middleware/responsehandler/AddressManagerResponseHandler.java
@@ -51,7 +51,7 @@ public class AddressManagerResponseHandler {
             notificationValidationActionHandler.handleValidateAndNormalizeAddressResponse(iun, normalizeItemsResult, communicationType);
             log.logEndingProcess(processName);
         } catch (Exception ex){
-            log.logEndingProcess(processName);
+            log.logEndingProcess(processName, false, ex.getMessage(),ex);
             throw ex;
         }
         
